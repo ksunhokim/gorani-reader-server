d872740bfbfe376ae63d21362a14999c01d53563
sunho <ksunhokim123@gmail.com>
2018-03-29 18:57:05 +0900

    edit: .gitignore

--- backend/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sunho/go/src/github.com/sunho/engbreaker/backend/src
-# Build directory: /Users/sunho/go/src/github.com/sunho/engbreaker/backend/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("app")
--- backend/build/app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sunho/go/src/github.com/sunho/engbreaker/backend/src/app
-# Build directory: /Users/sunho/go/src/github.com/sunho/engbreaker/backend/build/app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
