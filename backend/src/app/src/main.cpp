dab21214cc44c073a5e776816f5d6cff85f05348
sunho <ksunhokim123@gmail.com>
2018-03-29 23:32:56 +0900

    set: c++ project

--- backend/src/app/src/main.cpp
+++ backend/src/app/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cpp_redis/cpp_redis>
+#include <SimpleAmqpClient/SimpleAmqpClient.h>
 
 int main(int argc, char *argv[]) {
+	AmqpClient::Channel::ptr_t connection = AmqpClient::Channel::Create("localhost");	
 }
