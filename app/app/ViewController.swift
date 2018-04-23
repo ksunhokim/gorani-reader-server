62b211a1f8318541e53854f7d07f0188c055995f
sunho <ksunhokim123@gmail.com>
2018-04-24 07:52:51 +0900

    add: app

---
+++ app/app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by sunho on 2018. 4. 22..
+//  Copyright © 2018년 sunho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ scripts/crawl/crawl.py
@@ -0,0 +1,3 @@
+import requests
+
+req = requests.get('https://beomi.github.io/beomi.github.io_old/')
